43c7d2970328c31c93ec75aca3e251adbe3de61c
Alejandroanso <alejandroanso@hotmail.com>
2025-03-07 11:51:44 +0100

    crear barra de navegación

---
+++ index.js
@@ -0,0 +1,648 @@
+/* Design System */
+
+position: relative;
+width: 1473px;
+height: 733px;
+
+background: #FFFFFF;
+
+
+/* Group 58 */
+
+position: absolute;
+width: 94.1px;
+height: 94.1px;
+left: 70px;
+top: 104px;
+
+
+
+/* Rectangle 1 */
+
+position: absolute;
+width: 94.1px;
+height: 94.1px;
+left: 70px;
+top: 104px;
+
+/* Primary/Default */
+background: #072AC8;
+border-radius: 27.3798px;
+
+
+/* 072AC8 */
+
+position: absolute;
+width: 43px;
+height: 14px;
+left: 96px;
+top: 144px;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+font-size: 12px;
+line-height: 14px;
+/* identical to box height */
+
+color: #FFFFFF;
+
+
+
+/* Color Styles */
+
+position: absolute;
+width: 131px;
+height: 28px;
+left: 70px;
+top: 52px;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 700;
+font-size: 24px;
+line-height: 28px;
+text-align: right;
+
+color: #524E79;
+
+
+
+/* Text Styles */
+
+position: absolute;
+width: 120px;
+height: 28px;
+left: 759px;
+top: 52px;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 700;
+font-size: 24px;
+line-height: 28px;
+
+color: #524E79;
+
+
+
+/* Heading H1 60 Bold */
+
+position: absolute;
+width: 538px;
+height: 70px;
+left: 758px;
+top: 104px;
+
+/* H1 */
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 700;
+font-size: 60px;
+line-height: 70px;
+
+color: #514D67;
+
+
+
+/* Heading H2 50 Bold */
+
+position: absolute;
+width: 448px;
+height: 59px;
+left: 759px;
+top: 198px;
+
+/* H2 */
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 700;
+font-size: 50px;
+line-height: 59px;
+/* identical to box height */
+
+color: #514D67;
+
+
+
+/* Heading H3 40 Bold */
+
+position: absolute;
+width: 359px;
+height: 47px;
+left: 759px;
+top: 281px;
+
+/* H3 */
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 700;
+font-size: 40px;
+line-height: 47px;
+/* identical to box height */
+
+color: #514D67;
+
+
+
+/* Heading H4 24 Bold */
+
+position: absolute;
+width: 215px;
+height: 28px;
+left: 759px;
+top: 352px;
+
+/* H4 */
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 700;
+font-size: 24px;
+line-height: 28px;
+
+color: #514D67;
+
+
+
+/* Body Intro Text 24 Medium */
+
+position: absolute;
+width: 289px;
+height: 28px;
+left: 759px;
+top: 404px;
+
+/* Body/Medium */
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 500;
+font-size: 24px;
+line-height: 28px;
+
+color: #514D67;
+
+
+
+/* Body Intro Text 20 Regular */
+
+position: absolute;
+width: 234px;
+height: 30px;
+left: 759px;
+top: 456px;
+
+/* Body/Regular */
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 150%;
+/* identical to box height, or 30px */
+
+color: #514D67;
+
+
+
+/* Headline Text 17 Regular */
+
+position: absolute;
+width: 188px;
+height: 26px;
+left: 759px;
+top: 510px;
+
+/* Headline/Regular */
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+font-size: 17px;
+line-height: 150%;
+/* identical to box height, or 26px */
+
+color: #514D67;
+
+
+
+/* Headline Text 17 Medium */
+
+position: absolute;
+width: 194px;
+height: 22px;
+left: 759px;
+top: 554px;
+
+/* Headline/Medium */
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 500;
+font-size: 17px;
+line-height: 130%;
+/* identical to box height, or 22px */
+
+color: #514D67;
+
+
+
+/* Caption 1 Text 15 Regular */
+
+position: absolute;
+width: 171px;
+height: 18px;
+left: 759px;
+top: 600px;
+
+/* Caption/1 */
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+font-size: 15px;
+line-height: 18px;
+/* identical to box height */
+
+color: #514D67;
+
+
+
+/* Caption 2 Text 13 Regular */
+
+position: absolute;
+width: 149px;
+height: 15px;
+left: 759px;
+top: 642px;
+
+/* Caption/2 */
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+font-size: 13px;
+line-height: 15px;
+
+color: #514D67;
+
+
+
+/* Line 1 */
+
+position: absolute;
+width: 153px;
+height: 0px;
+left: 70px;
+top: 86px;
+
+border: 1px solid #DEDEDE;
+
+
+/* Line 4 */
+
+position: absolute;
+width: 135px;
+height: 0px;
+left: 759px;
+top: 86px;
+
+border: 1px solid #DEDEDE;
+
+
+/* Group 57 */
+
+position: absolute;
+width: 94.1px;
+height: 94.1px;
+left: 175px;
+top: 104px;
+
+
+
+/* Rectangle 6 */
+
+position: absolute;
+width: 94.1px;
+height: 94.1px;
+left: 175px;
+top: 104px;
+
+/* Accent */
+background: #43D2FF;
+border-radius: 27.3798px;
+
+
+/* 43D2FF */
+
+position: absolute;
+width: 42px;
+height: 14px;
+left: 202px;
+top: 144px;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+font-size: 12px;
+line-height: 14px;
+/* identical to box height */
+text-align: center;
+
+color: #FFFFFF;
+
+
+
+/* Group 56 */
+
+position: absolute;
+width: 94.1px;
+height: 94.1px;
+left: 280.9px;
+top: 104px;
+
+
+
+/* Rectangle 4 */
+
+position: absolute;
+width: 94.1px;
+height: 94.1px;
+left: 280.9px;
+top: 104px;
+
+/* Primary/Hover */
+background: #A2D6F9;
+border-radius: 27.3798px;
+
+
+/* A2D6F9 */
+
+position: absolute;
+width: 43px;
+height: 14px;
+left: 307px;
+top: 144px;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+font-size: 12px;
+line-height: 14px;
+/* identical to box height */
+text-align: center;
+
+color: #292E47;
+
+
+
+/* Group 55 */
+
+position: absolute;
+width: 94.1px;
+height: 94.1px;
+left: 388px;
+top: 104px;
+
+
+
+/* Rectangle 9 */
+
+position: absolute;
+width: 94.1px;
+height: 94.1px;
+left: 388px;
+top: 104px;
+
+/* Primary/Tapped */
+background: #D1EDFF;
+border-radius: 27.3798px;
+
+
+/* D1EDFF */
+
+position: absolute;
+width: 43px;
+height: 14px;
+left: 414.1px;
+top: 144px;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+font-size: 12px;
+line-height: 14px;
+/* identical to box height */
+text-align: center;
+
+color: #292E47;
+
+
+
+/* Group 64 */
+
+position: absolute;
+width: 94.1px;
+height: 94.1px;
+left: 388px;
+top: 217px;
+
+
+
+/* Rectangle 10 */
+
+position: absolute;
+width: 94.1px;
+height: 94.1px;
+left: 388px;
+top: 217px;
+
+/* Neutral/Light Blue */
+background: #ECF7FF;
+border-radius: 27.3798px;
+
+
+/* ECF7FF */
+
+position: absolute;
+width: 42px;
+height: 14px;
+left: 414.1px;
+top: 257px;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+font-size: 12px;
+line-height: 14px;
+/* identical to box height */
+text-align: center;
+
+color: #292E47;
+
+
+
+/* Group 59 */
+
+position: absolute;
+width: 94.1px;
+height: 94.1px;
+left: 70px;
+top: 216.75px;
+
+
+
+/* Rectangle 3 */
+
+position: absolute;
+width: 94.1px;
+height: 94.1px;
+left: 70px;
+top: 216.75px;
+
+/* Neutral/Dark */
+background: #292E47;
+border-radius: 27.3798px;
+
+
+/* 292E47 */
+
+position: absolute;
+width: 41px;
+height: 14px;
+left: 97px;
+top: 256px;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+font-size: 12px;
+line-height: 14px;
+/* identical to box height */
+text-align: center;
+
+color: #FFFFFF;
+
+
+
+/* Group 62 */
+
+position: absolute;
+width: 94.1px;
+height: 94.1px;
+left: 70px;
+top: 335px;
+
+
+
+/* Rectangle 8 */
+
+position: absolute;
+width: 94.1px;
+height: 94.1px;
+left: 70px;
+top: 335px;
+
+/* Secondary/1 */
+background: #FFC600;
+border-radius: 27.3798px;
+
+
+/* FFC600 */
+
+position: absolute;
+width: 42px;
+height: 14px;
+left: 96px;
+top: 375px;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+font-size: 12px;
+line-height: 14px;
+/* identical to box height */
+text-align: center;
+
+color: #292E47;
+
+
+
+/* Group 60 */
+
+position: absolute;
+width: 94.1px;
+height: 94.1px;
+left: 175px;
+top: 217px;
+
+
+
+/* Rectangle 7 */
+
+position: absolute;
+width: 94.1px;
+height: 94.1px;
+left: 175px;
+top: 217px;
+
+/* Neutral/Medium */
+background: #6B708D;
+border-radius: 27.3798px;
+
+
+/* 6B708D */
+
+position: absolute;
+width: 43px;
+height: 14px;
+left: 202px;
+top: 256px;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+font-size: 12px;
+line-height: 14px;
+/* identical to box height */
+text-align: center;
+
+color: #FFFFFF;
+
+
+
+/* Group 61 */
+
+position: absolute;
+width: 94.1px;
+height: 94.1px;
+left: 280px;
+top: 216.75px;
+
+
+
+/* Rectangle 5 */
+
+position: absolute;
+width: 94.1px;
+height: 94.1px;
+left: 280px;
+top: 216.75px;
+
+/* Neutral/Light */
+background: #F4F6FC;
+border-radius: 27.3798px;
+
+
+/* F2F4FC */
+
+position: absolute;
+width: 42px;
+height: 14px;
+left: 306px;
+top: 256px;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+font-size: 12px;
+line-height: 14px;
+/* identical to box height */
+text-align: center;
+
+color: #514D67;
+
